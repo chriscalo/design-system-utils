8a67133d21818f8b3ced0f2ed9c949ea6844abae
Chris Calo <chris.calo@gmail.com>
2019-01-16 22:28:58 -0500

    Add script to generate SCSS file of colors

---
+++ src/generate-colors.js
@@ -0,0 +1,24 @@
+const { scales } = require("./color-scales");
+
+const fs = require("fs-extra");
+const path = require("path");
+
+const outputFile = path.join(__dirname, "../dist/colors.scss");
+const output = [];
+
+Object.entries(scales).forEach(([colorName, scale]) => {
+  // empty line between colors
+  output.push(``);
+  output.push(`// ${scale.colorName} color scale:`);
+  const values = [
+    5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
+  ];
+  const scss = values.map(value => `$${colorName}-${value}: ${scale(value)};`);
+  output.push(scss.join("\n"));
+});
+
+// for new line ending
+output.push(``);
+
+const content = output.join("\n");
+fs.outputFileSync(outputFile, content);
